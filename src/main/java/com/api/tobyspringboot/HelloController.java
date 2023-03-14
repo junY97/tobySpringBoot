eea6b6c70447d3573f41aabd2e9ab68003c00b4f
junyeong.jo <whwnsdud2024@naver.com>
2023-03-14 17:00:38 +0900

    애노테이션 매핑 정보 사용

--- src/main/java/com/api/tobyspringboot/HelloController.java
+++ src/main/java/com/api/tobyspringboot/HelloController.java
@@ -1,18 +1,26 @@
 package com.api.tobyspringboot;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
 import java.util.Objects;
 
 /**
  * @author junyeong.jo .
  * @since 2023-03-14
  */
+@Controller
+@RequestMapping("/hello")
 public class HelloController {
     private final HelloService helloService;
 
     public HelloController(HelloService helloService) {
         this.helloService = helloService;
     }
-
+    @GetMapping
+    @ResponseBody
     public String hello(String name) {
         return helloService.sayHello(Objects.requireNonNull(name));
     }
