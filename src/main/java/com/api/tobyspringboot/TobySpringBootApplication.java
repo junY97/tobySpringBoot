4a21fcf07e81a2f8fb111509316e33b641d83b21
junyeong.jo <whwnsdud2024@naver.com>
2023-03-14 16:58:46 +0900

    DispatcherServlet으로 전환

--- src/main/java/com/api/tobyspringboot/TobySpringBootApplication.java
+++ src/main/java/com/api/tobyspringboot/TobySpringBootApplication.java
@@ -1,51 +1,24 @@
 package com.api.tobyspringboot;
 
-import jakarta.servlet.ServletException;
-import jakarta.servlet.http.HttpServlet;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
-import org.springframework.context.support.GenericApplicationContext;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-
-import java.io.IOException;
+import org.springframework.web.context.support.GenericWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
 
 public class TobySpringBootApplication {
 
     public static void main(String[] args) {
-        GenericApplicationContext applicationContext = new GenericApplicationContext(); // 어플리케이션 컨텍스트 : 스프링 컨테이너 구성
+        GenericWebApplicationContext applicationContext = new GenericWebApplicationContext(); // 어플리케이션 컨텍스트 : 스프링 컨테이너 구성
         applicationContext.registerBean(HelloController.class); // 빈 등록
-        applicationContext.registerBean(HelloService.class); // 빈 등록
+        applicationContext.registerBean(SimpleHelloService.class); // 빈 등록
         applicationContext.refresh(); // 컨테이너 초기화
 
         ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
         WebServer webServer = serverFactory.getWebServer(servletContext -> {
-            servletContext.addServlet("frontcontroller", new HttpServlet() {
-                @Override
-                protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-
-                if(req.getRequestURI().equals("/hello") && req.getMethod().equals(HttpMethod.GET.name())) {
-                    String name = req.getParameter("name");
-                    HelloController helloController = applicationContext.getBean(HelloController.class);
-                    String ret = helloController.hello(name);
-
-                    resp.setStatus(HttpStatus.OK.value());
-                    resp.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE);
-                    resp.getWriter().println(ret);
-                }
-                else if (req.getRequestURI().equals("/user")) {
-
-                }
-                else {
-                    resp.setStatus(HttpStatus.NOT_FOUND.value());
-                    }
-                }
-            }).addMapping("/*");
+            servletContext.addServlet("DispatcherServlet",
+                    new DispatcherServlet(applicationContext)
+            ).addMapping("/*");
         });
         webServer.start();
     }
