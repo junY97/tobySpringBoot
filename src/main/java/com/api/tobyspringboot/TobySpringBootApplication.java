93c3b7f326cfedea589057f436c66738acaee1cb
junyeong.jo <whwnsdud2024@naver.com>
2023-03-14 10:42:34 +0900

    initial commit

---
+++ src/main/java/com/api/tobyspringboot/TobySpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.api.tobyspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TobySpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TobySpringBootApplication.class, args);
+    }
+
+}
