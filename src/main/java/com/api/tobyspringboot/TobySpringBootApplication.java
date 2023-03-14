63787c5a8a6a1ee0a448772eae7de14fd8920bab
junyeong.jo <whwnsdud2024@naver.com>
2023-03-14 17:11:14 +0900

    스프링 컨테이너로 통합

--- src/main/java/com/api/tobyspringboot/TobySpringBootApplication.java
+++ src/main/java/com/api/tobyspringboot/TobySpringBootApplication.java
@@ -9,18 +9,22 @@ import org.springframework.web.servlet.DispatcherServlet;
 public class TobySpringBootApplication {
 
     public static void main(String[] args) {
-        GenericWebApplicationContext applicationContext = new GenericWebApplicationContext(); // 어플리케이션 컨텍스트 : 스프링 컨테이너 구성
+        GenericWebApplicationContext applicationContext = new GenericWebApplicationContext() {
+            @Override
+            protected void onRefresh() {
+                super.onRefresh();
+                ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
+                WebServer webServer = serverFactory.getWebServer(servletContext -> {
+                    servletContext.addServlet("DispatcherServlet",
+                            new DispatcherServlet(this)
+                    ).addMapping("/*");
+                });
+                webServer.start();
+            }
+        };
         applicationContext.registerBean(HelloController.class); // 빈 등록
         applicationContext.registerBean(SimpleHelloService.class); // 빈 등록
         applicationContext.refresh(); // 컨테이너 초기화
-
-        ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
-        WebServer webServer = serverFactory.getWebServer(servletContext -> {
-            servletContext.addServlet("DispatcherServlet",
-                    new DispatcherServlet(applicationContext)
-            ).addMapping("/*");
-        });
-        webServer.start();
     }
 
 }
