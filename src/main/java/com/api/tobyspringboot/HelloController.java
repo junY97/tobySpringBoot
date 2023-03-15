7403cfaafaffa73fd3f8b82556ad978fcc106c8f
junyeong.jo <whwnsdud2024@naver.com>
2023-03-15 12:22:54 +0900

    Bean의 생명주기 메소드

--- src/main/java/com/api/tobyspringboot/HelloController.java
+++ src/main/java/com/api/tobyspringboot/HelloController.java
@@ -1,5 +1,6 @@
 package com.api.tobyspringboot;
 
+import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,13 +16,21 @@ import java.util.Objects;
 @RequestMapping("/hello")
 public class HelloController {
     private final HelloService helloService;
+    private final ApplicationContext applicationContext;
 
-    public HelloController(HelloService helloService) {
+
+    public HelloController(HelloService helloService, ApplicationContext applicationContext) {
         this.helloService = helloService;
+        this.applicationContext = applicationContext;
     }
     @GetMapping
     @ResponseBody
     public String hello(String name) {
         return helloService.sayHello(Objects.requireNonNull(name));
     }
+
+//    @Override
+//    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+//        System.out.println(applicationContext);
+//    }
 }
--- src/main/java/com/api/tobyspringboot/TobySpringBootApplication.java
+++ src/main/java/com/api/tobyspringboot/TobySpringBootApplication.java
@@ -3,6 +3,7 @@ package com.api.tobyspringboot;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
@@ -10,15 +11,23 @@ import org.springframework.web.servlet.DispatcherServlet;
 @Configuration
 @ComponentScan
 public class TobySpringBootApplication {
+    @Bean
+    public ServletWebServerFactory servletWebServerFactory() {
+        return new TomcatServletWebServerFactory();
+    }
+    @Bean
+    public DispatcherServlet dispatcherServlet() {
+        return new DispatcherServlet();
+    }
     public static void main(String[] args) {
         AnnotationConfigWebApplicationContext applicationContext = new AnnotationConfigWebApplicationContext() {
             @Override
             protected void onRefresh() {
                 super.onRefresh();
-                ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
+                ServletWebServerFactory serverFactory = this.getBean(ServletWebServerFactory.class);
+                DispatcherServlet dispatcherServlet = this.getBean(DispatcherServlet.class);
                 WebServer webServer = serverFactory.getWebServer(servletContext -> {
-                    servletContext.addServlet("DispatcherServlet",
-                            new DispatcherServlet(this)
+                    servletContext.addServlet("DispatcherServlet", dispatcherServlet
                     ).addMapping("/*");
                 });
                 webServer.start();
