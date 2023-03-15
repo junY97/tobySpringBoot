14033f87f54cd630c224cefdff223df430ccfaf3
junyeong.jo <whwnsdud2024@naver.com>
2023-03-15 09:58:04 +0900

    @Component 스캔

--- src/main/java/com/api/tobyspringboot/HelloService.java
+++ src/main/java/com/api/tobyspringboot/HelloService.java
@@ -4,6 +4,7 @@ package com.api.tobyspringboot;
  * @author junyeong.jo .
  * @since 2023-03-14
  */
+
 public interface HelloService {
     String sayHello(String name);
 }
--- src/main/java/com/api/tobyspringboot/SimpleHelloService.java
+++ src/main/java/com/api/tobyspringboot/SimpleHelloService.java
@@ -1,9 +1,12 @@
 package com.api.tobyspringboot;
 
+import org.springframework.stereotype.Component;
+
 /**
  * @author junyeong.jo .
  * @since 2023-03-14
  */
+@Component
 public class SimpleHelloService implements HelloService {
     @Override
     public String sayHello(String name) {
