ff7a55297831e6235099fdce304416c61d9681a0
junyeong.jo <whwnsdud2024@naver.com>
2023-03-21 17:55:37 +0900

    @Conditional과 Condition

--- src/main/java/com/api/tobyspringboot/config/autoconfig/JettyWebServerConfig.java
+++ src/main/java/com/api/tobyspringboot/config/autoconfig/JettyWebServerConfig.java
@@ -4,15 +4,27 @@ import com.api.tobyspringboot.config.MyAutoConfiguration;
 import org.springframework.boot.web.embedded.jetty.JettyServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Condition;
+import org.springframework.context.annotation.ConditionContext;
+import org.springframework.context.annotation.Conditional;
+import org.springframework.core.type.AnnotatedTypeMetadata;
 
 /**
  * @author junyeong.jo .
  * @since 2023-03-21
  */
 @MyAutoConfiguration
+@Conditional(JettyWebServerConfig.JettyCondition.class)
 public class JettyWebServerConfig {
     @Bean("JettyWebServerConfig")
     public ServletWebServerFactory servletWebServerFactory() {
         return new JettyServletWebServerFactory();
+    }
+    static class JettyCondition implements Condition {
+
+        @Override
+        public boolean matches(ConditionContext context, AnnotatedTypeMetadata metadata) {
+            return true;
+        }
     }
 }
